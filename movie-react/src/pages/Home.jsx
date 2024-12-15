ef5d2625f3627b34308a0b5854e19aa9a90738bc
dichev-design <dichev.official@gmail.com>
2024-12-15 10:36:59 +0200

    Home Page Rendering, and searchState Set

---
+++ movie-react/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Harry Potter", release_date: "2003" },
+    { id: 3, title: "Terminator", release_date: "1998" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movie-grid">
+        {movies.map(
+          (movie) =>
+            movie.title.toLocaleLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
